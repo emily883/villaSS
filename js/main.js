8efd1bda04244c6f93d9ee155fb7d19dcdb2cd30
Emily Cruz <emily.moraima.cruz@hotmail.com>
2022-01-26 18:49:11 -0500

    Landing page Villa Sol Naciente

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+$(document).on('click', 'a[href^="#"]', function (e) {
+	e.preventDefault();
+
+	$('html, body').animate({
+		scrollTop: $($.attr(this, 'href')).offset().top
+	}, 900);
+});
+
+// Esto es para copiar la ip del servidor si es que aplica :v 
+
+// let t;
+// $(document).ready(()=>{
+// 	t = $(".ip").html();
+// })
+// $(document).on("click",".ip",()=>{
+// 	let copy = document.createElement("textarea");
+// 	copy.style.position = "absolute";
+// 	copy.style.left = "-99999px";
+// 	copy.style.top = "0";
+// 	copy.setAttribute("id", "ta");
+// 	document.body.appendChild(copy);
+// 	copy.textContent = t;
+// 	copy.select();
+// 	document.execCommand("copy");
+// 	$(".ip").html("<span class='extrapad'>IP copied!</span>");
+// 	setTimeout(function(){
+// 		$(".ip").html(t);
+// 		var copy = document.getElementById("ta");
+// 		copy.parentNode.removeChild(copy);
+// 	},1000);
+// });
